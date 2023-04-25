3877ae76b325af9d80c3e651cb5cf4154e35fc46
Cho SangHyun <98shcho@naver.com>
2023-04-25 20:32:53 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/swipkkun/swipkkun/SwipkkunApplication.java
@@ -0,0 +1,13 @@
+package swipkkun.swipkkun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwipkkunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwipkkunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swipkkun/swipkkun/SwipkkunApplicationTests.java
@@ -0,0 +1,13 @@
+package swipkkun.swipkkun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipkkunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
