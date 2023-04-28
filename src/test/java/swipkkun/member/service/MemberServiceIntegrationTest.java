2dffd75fc4dda6d4df8f6e78f0b8aebf87673a85
Cho SangHyun <98shcho@naver.com>
2023-04-28 19:23:49 +0900

    test: 회원가입 테스트 작성

---
+++ src/test/java/swipkkun/member/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,37 @@
+package swipkkun.member.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import swipkkun.member.domain.Member;
+import swipkkun.member.repository.MemberRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Nested
+    class SignupTest {
+        @DisplayName("회원가입 테스트")
+        @Test
+        void signup() {
+            Member member = new Member();
+            member.setEmail("test@google.com");
+            member.setPassword("123213");
+            member.setNickname("구우욷");
+            member.setPhone("010-2134-4321");
+
+            memberService.signup(member);
+            Member joinedMember = memberService.findByEmail("test@google.com").get();
+
+            assertThat(joinedMember.getEmail()).isEqualTo(member.getEmail());
+        }
+    }
+}
