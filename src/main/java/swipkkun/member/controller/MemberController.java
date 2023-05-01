f8d8eb829ff10e93d9ae86c298fc0ee3cdfd54d3
Cho SangHyun <98shcho@naver.com>
2023-05-01 22:47:03 +0900

    feat: 로그인 기능 간단하게 구현

--- src/main/java/swipkkun/member/controller/MemberController.java
+++ src/main/java/swipkkun/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import swipkkun.member.dto.LoginRequestDto;
 import swipkkun.member.dto.SignupRequestDto;
 import swipkkun.member.service.MemberService;
 
@@ -15,6 +16,12 @@ public class MemberController {
     @PostMapping("/signup")
     public String signup(@RequestBody SignupRequestDto requestDto) {
         memberService.signup(requestDto);
+        return "success";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequestDto requestDto) {
+        memberService.login(requestDto);
         return "success";
     }
 }
--- src/main/java/swipkkun/member/service/MemberService.java
+++ src/main/java/swipkkun/member/service/MemberService.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import swipkkun.member.domain.Member;
+import swipkkun.member.dto.LoginRequestDto;
 import swipkkun.member.dto.SignupRequestDto;
 import swipkkun.member.repository.MemberRepository;
 
@@ -25,6 +26,13 @@ public class MemberService {
         member.setNickname(requestDto.getNickname());
         member.setPhone(requestDto.getPhone());
         memberRepository.save(member);
+    }
+
+    public void login(LoginRequestDto requestDto) {
+        Member member = findByEmail(requestDto.getEmail());
+        if (!passwordEncoder.matches(requestDto.getPassword(), member.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 잘못됐습니다!!");
+        }
     }
 
     public Member findByEmail(String email) {
