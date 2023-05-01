6faa17345af9ec49debde069c347200dba1832c7
Cho SangHyun <98shcho@naver.com>
2023-05-01 22:58:29 +0900

    test: 로그인 관련 테스트 추가

--- src/test/java/swipkkun/member/service/MemberServiceIntegrationTest.java
+++ src/test/java/swipkkun/member/service/MemberServiceIntegrationTest.java
@@ -7,9 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import swipkkun.member.domain.Member;
+import swipkkun.member.dto.LoginRequestDto;
 import swipkkun.member.dto.SignupRequestDto;
 import swipkkun.member.repository.MemberRepository;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -33,6 +38,47 @@ class MemberServiceIntegrationTest {
             Member joinedMember = memberService.findByEmail("test@google.com");
 
             assertThat(joinedMember.getEmail()).isEqualTo(requestDto.getEmail());
+        }
+    }
+
+    @Nested
+    class LoginTest {
+        @DisplayName("로그인 테스트")
+        @Test
+        void login() {
+            SignupRequestDto signupRequestDto = new SignupRequestDto();
+            signupRequestDto.setEmail("test@google.com");
+            signupRequestDto.setPassword("123213");
+            signupRequestDto.setNickname("구우욷");
+            signupRequestDto.setPhone("010-2134-4321");
+
+            memberService.signup(signupRequestDto);
+
+            LoginRequestDto loginRequestDto = new LoginRequestDto();
+            loginRequestDto.setEmail("test@google.com");
+            loginRequestDto.setPassword("123213");
+
+            assertThatCode(() -> memberService.login(loginRequestDto))
+                    .doesNotThrowAnyException();
+        }
+
+        @DisplayName("로그인할 때 비번틀리면 오류난다")
+        @Test
+        void login2() {
+            SignupRequestDto signupRequestDto = new SignupRequestDto();
+            signupRequestDto.setEmail("test@google.com");
+            signupRequestDto.setPassword("123213");
+            signupRequestDto.setNickname("구우욷");
+            signupRequestDto.setPhone("010-2134-4321");
+
+            memberService.signup(signupRequestDto);
+
+            LoginRequestDto loginRequestDto = new LoginRequestDto();
+            loginRequestDto.setEmail("test@google.com");
+            loginRequestDto.setPassword("123212");
+
+            assertThatThrownBy(() -> memberService.login(loginRequestDto))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
