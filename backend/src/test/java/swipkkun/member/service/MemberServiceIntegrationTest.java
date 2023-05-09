336027e058f4e2ca9ba33e393b38f8815dbf7301
Cho SangHyun <98shcho@naver.com>
2023-05-10 02:32:17 +0900

    test: 회원가입/로그인 테스트 코드 수정
    테스트용 이메일 변경

--- backend/src/test/java/swipkkun/member/service/MemberServiceIntegrationTest.java
+++ backend/src/test/java/swipkkun/member/service/MemberServiceIntegrationTest.java
@@ -29,13 +29,13 @@ class MemberServiceIntegrationTest {
         @Test
         void signup() {
             SignupRequestDto requestDto = new SignupRequestDto();
-            requestDto.setEmail("test@google.com");
+            requestDto.setEmail("test1@google.com");
             requestDto.setPassword("123213");
             requestDto.setNickname("구우욷");
             requestDto.setPhone("010-2134-4321");
 
             memberService.signup(requestDto);
-            Member joinedMember = memberService.findByEmail("test@google.com");
+            Member joinedMember = memberService.findByEmail("test1@google.com");
 
             assertThat(joinedMember.getEmail()).isEqualTo(requestDto.getEmail());
         }
@@ -47,7 +47,7 @@ class MemberServiceIntegrationTest {
         @Test
         void login() {
             SignupRequestDto signupRequestDto = new SignupRequestDto();
-            signupRequestDto.setEmail("test@google.com");
+            signupRequestDto.setEmail("test1@google.com");
             signupRequestDto.setPassword("123213");
             signupRequestDto.setNickname("구우욷");
             signupRequestDto.setPhone("010-2134-4321");
@@ -55,7 +55,7 @@ class MemberServiceIntegrationTest {
             memberService.signup(signupRequestDto);
 
             LoginRequestDto loginRequestDto = new LoginRequestDto();
-            loginRequestDto.setEmail("test@google.com");
+            loginRequestDto.setEmail("test1@google.com");
             loginRequestDto.setPassword("123213");
 
             assertThatCode(() -> memberService.login(loginRequestDto))
@@ -66,7 +66,7 @@ class MemberServiceIntegrationTest {
         @Test
         void login2() {
             SignupRequestDto signupRequestDto = new SignupRequestDto();
-            signupRequestDto.setEmail("test@google.com");
+            signupRequestDto.setEmail("test1@google.com");
             signupRequestDto.setPassword("123213");
             signupRequestDto.setNickname("구우욷");
             signupRequestDto.setPhone("010-2134-4321");
@@ -74,7 +74,7 @@ class MemberServiceIntegrationTest {
             memberService.signup(signupRequestDto);
 
             LoginRequestDto loginRequestDto = new LoginRequestDto();
-            loginRequestDto.setEmail("test@google.com");
+            loginRequestDto.setEmail("test1@google.com");
             loginRequestDto.setPassword("123212");
 
             assertThatThrownBy(() -> memberService.login(loginRequestDto))
