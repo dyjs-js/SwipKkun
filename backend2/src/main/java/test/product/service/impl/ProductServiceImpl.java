fe490e8f8471d219eff22c3d0626e145a1b53263
kjk4867 <127995835+kjk4867@users.noreply.github.com>
2023-05-17 20:21:22 +0900

    상품 목록/갯수/정보/조회수 업데이트/리뷰 목록 ServiceImpl단 추가

---
+++ backend2/src/main/java/test/product/service/impl/ProductServiceImpl.java
@@ -0,0 +1,101 @@
+package test.product.service.impl;
+
+import java.util.HashMap;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import test.product.service.ProductService;
+import test.product.service.ProductVO;
+
+@Service("productService")
+public class ProductServiceImpl extends EgovAbstractServiceImpl implements ProductService {
+	
+	/** productDAO */
+	@Resource(name = "productDAO")
+	private ProductDAO productDAO;
+	
+	/**
+	 * 상품 목록
+	 * @param vo - 조회할 정보가 담긴 CodeVO
+	 * @return 조회 데이터
+	 * @exception Exception
+	 */
+	public List<ProductVO> selectProductList(ProductVO vo) throws Exception {
+		return productDAO.selectProductList(vo);
+	}
+	
+	/**
+	 * 상품 갯수
+	 * @param vo - 갯수가 담긴 ProductVO
+	 * @return 갯수 데이터
+	 * @exception Exception
+	 */
+	public int selectProductListCnt(ProductVO vo) throws Exception {
+		return productDAO.selectProductListCnt(vo);
+	}
+	
+	/**
+	 * 상품 정보
+	 * @param vo - 등록할 정보가 담긴 ProductVO
+	 * @return 등록 데이터
+	 * @exception Exception
+	 */
+	public ProductVO getProduct(ProductVO vo) throws Exception {
+		return productDAO.getProduct(vo);
+	}
+	
+	/**
+	 * 상품 조회수 업데이트
+	 * @param vo - 수정할 정보가 담긴 ProductVO
+	 * @return 수정 데이터
+	 * @exception Exception
+	 */
+	public int updateProductHitCnt(ProductVO vo) throws Exception {
+		return productDAO.updateProductHitCnt(vo);
+	}
+	
+	/**
+	 * 상품 리뷰 목록
+	 * @param vo - 조회할 정보가 담긴 CodeVO
+	 * @return 조회 데이터
+	 * @exception Exception
+	 */
+	public List<ProductVO> selectProductReviewList(ProductVO vo) throws Exception {
+		return productDAO.selectProductReviewList(vo);
+	}
+	
+	/**
+	 * 상품 등록
+	 * @param vo - 등록할 정보가 담긴 ProductVO
+	 * @return 등록 데이터
+	 * @exception Exception
+	 */
+	public int insertProduct(HashMap<String, Object> map) throws Exception {
+		return productDAO.insertProduct(map);
+	}
+	
+	/**
+	 * 상품 수정
+	 * @param vo - 수정할 정보가 담긴 ProductVO
+	 * @return 수정 데이터
+	 * @exception Exception
+	 */
+	public int updateProduct(HashMap<String, Object> map) throws Exception {
+		return productDAO.updateProduct(map);
+	}
+	
+	/**
+	 * 상품 삭제
+	 * @param vo - 삭제할 정보가 담긴 ProductVO
+	 * @return 삭제 데이터
+	 * @exception Exception
+	 */
+	public int deleteProduct(HashMap<String, Object> map) throws Exception {
+		return productDAO.deleteProduct(map);
+	}
+	
+}
