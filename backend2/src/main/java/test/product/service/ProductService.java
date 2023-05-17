c05eccc81fd86a3c0848bb931cb66f7b4ab05cf4
kjk4867 <127995835+kjk4867@users.noreply.github.com>
2023-05-17 20:18:56 +0900

    상품 목록/갯수/정보/조회수 업데이트/리뷰 목록 service단 추가

---
+++ backend2/src/main/java/test/product/service/ProductService.java
@@ -0,0 +1,72 @@
+package test.product.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface ProductService {
+	
+	/**
+	 * 상품 목록
+	 * @param vo - 조회할 정보가 담긴 CodeVO
+	 * @return 조회 데이터
+	 * @exception Exception
+	 */
+	List<ProductVO> selectProductList(ProductVO vo) throws Exception;
+	
+	/**
+	 * 상품 갯수
+	 * @param vo - 갯수가 담긴 ProductVO
+	 * @return 갯수 데이터
+	 * @exception Exception
+	 */
+	int selectProductListCnt(ProductVO vo) throws Exception;
+	
+	/**
+	 * 상품 정보
+	 * @param vo - 등록할 정보가 담긴 ProductVO
+	 * @return 등록 데이터
+	 * @exception Exception
+	 */
+	ProductVO getProduct(ProductVO vo) throws Exception;
+	
+	/**
+	 * 상품 조회수 업데이트
+	 * @param vo - 수정할 정보가 담긴 ProductVO
+	 * @return 수정 데이터
+	 * @exception Exception
+	 */
+	int updateProductHitCnt(ProductVO vo) throws Exception;
+	
+	/**
+	 * 상품 리뷰 목록
+	 * @param vo - 조회할 정보가 담긴 CodeVO
+	 * @return 조회 데이터
+	 * @exception Exception
+	 */
+	List<ProductVO> selectProductReviewList(ProductVO vo) throws Exception;
+	
+	/**
+	 * 상품 등록
+	 * @param vo - 등록할 정보가 담긴 ProductVO
+	 * @return 등록 데이터
+	 * @exception Exception
+	 */
+	int insertProduct(HashMap<String, Object> map) throws Exception;
+	
+	/**
+	 * 상품 수정
+	 * @param vo - 수정할 정보가 담긴 ProductVO
+	 * @return 수정 데이터
+	 * @exception Exception
+	 */
+	int updateProduct(HashMap<String, Object> map) throws Exception;
+	
+	/**
+	 * 상품 삭제
+	 * @param vo - 삭제할 정보가 담긴 ProductVO
+	 * @return 삭제 데이터
+	 * @exception Exception
+	 */
+	int deleteProduct(HashMap<String, Object> map) throws Exception;
+	
+}
