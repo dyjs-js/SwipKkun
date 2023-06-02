3352c473953477ecc32f2ddd7c8214f5e8f66059
kjk4867 <127995835+kjk4867@users.noreply.github.com>
2023-06-03 03:44:03 +0900

    Update ProductController.java

--- backend2/src/main/java/test/product/web/ProductController.java
+++ backend2/src/main/java/test/product/web/ProductController.java
@@ -1,6 +1,10 @@
 package test.product.web;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +15,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import egovframework.rte.fdl.property.EgovPropertyService;
 import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
@@ -48,8 +55,19 @@ public class ProductController {
 		productvo.setLastIndex(paginationInfo.getLastRecordIndex());
 		productvo.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
 		
-		if(productvo.getSearchOrderGb() == null) {
+		String searchOrderBy = request.getParameter("searchOrderBy");
+		String searchOrderGb = request.getParameter("searchOrderGb");
+		
+		System.out.println("searchOrderBy -->>> " + searchOrderBy);
+		System.out.println("searchOrderGb -->>> " + searchOrderGb);
+		
+		productvo.setSearchOrderBy(searchOrderBy);
+		productvo.setSearchOrderGb(searchOrderGb);
+		
+		if(searchOrderGb == null) {
 			productvo.setSearchOrderGb("DESC");
+		} else {
+			productvo.setSearchOrderGb(searchOrderGb);
 		}
 		
 		List<ProductVO> list = productService.selectProductList(productvo);
@@ -81,11 +99,225 @@ public class ProductController {
 			// 상품 리뷰 리ㅅ트
 			List<ProductVO> reviewList = productService.selectProductReviewList(productvo);
 			
+			// 비슷한 상품 리뷰 리스트
+			List<ProductVO> resultNotList = productService.selectProductNotList(productvo);
+			
 			model.addAttribute("result", result);
 			model.addAttribute("reviewList", reviewList);
+			model.addAttribute("resultNotList", resultNotList);
 		}
 		
 		return "/product/view";
+	}
+	
+	@RequestMapping(value = "/product/insert.do")
+	public String productInsert(
+		HttpServletRequest request, 
+		HttpServletResponse response, 
+		HttpSession session, 
+		ModelMap model, 
+		@ModelAttribute("productVO") ProductVO productvo 
+	) throws Exception {
+		return "/product/insert";
+	}
+	
+	@RequestMapping(value = "/product/edit.do")
+	public String productEdit(
+		HttpServletRequest request, 
+		HttpServletResponse response, 
+		HttpSession session, 
+		ModelMap model, 
+		@ModelAttribute("productVO") ProductVO productvo 
+	) throws Exception {
+		// 상품 조회수 업데이트
+		int updateHitCnt = productService.updateProductHitCnt(productvo);
+		
+		if(updateHitCnt > 0) {
+			// 상품 상세정보
+			ProductVO result = productService.getProduct(productvo);
+			
+			// 상품 리뷰 리ㅅ트
+			List<ProductVO> reviewList = productService.selectProductReviewList(productvo);
+			
+			// 비슷한 상품 리뷰 리스트
+			List<ProductVO> resultNotList = productService.selectProductNotList(productvo);
+			
+			model.addAttribute("result", result);
+			model.addAttribute("reviewList", reviewList);
+			model.addAttribute("resultNotList", resultNotList);
+		}
+		
+		return "/product/edit";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/product/fileDel.do")
+	public Map<String, Object> productFileDel(
+		HttpServletRequest request, 
+		HttpServletResponse response, 
+		HttpSession session, 
+		ModelMap model, 
+		@ModelAttribute("productVO") ProductVO productvo 
+	) throws Exception {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		
+		// 상품 조회수 업데이트
+		ProductVO result = productService.getProduct(productvo);
+		
+		if(result != null) {
+			File file = new File("C:/project3/test/WebContent" + result.getProduct_img());
+			
+			if(file.exists()) {
+				if(file.delete()) {
+					int imgDelCnt = productService.updateProductImgDel(productvo);
+					
+					if(imgDelCnt > 0) {
+						map.put("success", "true");
+					} else {
+						map.put("success", "false");
+					}
+				} else {
+					int imgDelCnt = productService.updateProductImgDel(productvo);
+					
+					if(imgDelCnt > 0) {
+						map.put("success", "true");
+					} else {
+						map.put("success", "false");
+					}
+				}
+			} else {
+				int imgDelCnt = productService.updateProductImgDel(productvo);
+				
+				if(imgDelCnt > 0) {
+					map.put("success", "true");
+				} else {
+					map.put("success", "false");
+				}
+			}
+		} else {
+			map.put("success", "false");
+		}
+		
+		return map;
+	}
+	
+	@RequestMapping(value = "/product/insert_proc.do")
+	public String productInsertProc(
+		MultipartHttpServletRequest request, 
+		HttpServletResponse response, 
+		HttpSession session, 
+		ModelMap model
+	) throws Exception {
+		// 상품정보 추가
+		ProductVO productvo = new ProductVO();
+		
+		productvo.setProduct_name(request.getParameter("product_name"));
+		productvo.setProduct_eng_name(request.getParameter("product_eng_name"));
+		productvo.setProduct_day_bak(request.getParameter("product_day_bak"));
+		productvo.setProduct_price(request.getParameter("product_price"));
+		
+		MultipartFile imgFile = null;
+		
+		try {
+			Iterator<String> iterator = request.getFileNames();
+			imgFile = null;
+			
+			String filePath = "C:\\project3\\test\\WebContent\\assets\\img";
+			
+			if(iterator.hasNext()) {
+				imgFile = request.getFile(iterator.next());
+			}
+			
+			if(imgFile != null && imgFile.getOriginalFilename() != null) {
+				productvo.setProduct_img("/assets/img/product/" + imgFile.getOriginalFilename());
+				
+				String saveFilePath = filePath + File.separator + imgFile.getOriginalFilename();
+				imgFile.transferTo(new File(saveFilePath));
+			}
+		} catch(Exception e) {
+			System.out.println("e ---->>> " + e.getMessage());
+		}
+		
+		productvo.setProduct_address(request.getParameter("product_address"));
+		productvo.setProduct_content(request.getParameter("product_content"));
+		productvo.setProduct_hash_tag(request.getParameter("product_hash_tag"));
+		
+		int insertCnt = productService.insertProduct(productvo);
+		
+		if(insertCnt > 0) {
+			return "redirect:/product/list.do";
+		} else {
+			return "redirect:/product/insert.do";
+		}
+	}
+	
+	@RequestMapping(value = "/product/edit_proc.do")
+	public String productEditProc(
+		MultipartHttpServletRequest request, 
+		HttpServletResponse response, 
+		HttpSession session, 
+		ModelMap model
+	) throws Exception {
+		// 상품정보 업데이트
+		ProductVO productvo = new ProductVO();
+		
+		productvo.setProduct_idx(Integer.parseInt(request.getParameter("product_idx")));
+		productvo.setProduct_name(request.getParameter("product_name"));
+		productvo.setProduct_eng_name(request.getParameter("product_eng_name"));
+		productvo.setProduct_day_bak(request.getParameter("product_day_bak"));
+		productvo.setProduct_price(request.getParameter("product_price"));
+		
+		MultipartFile imgFile = null;
+		
+		try {
+			Iterator<String> iterator = request.getFileNames();
+			imgFile = null;
+			
+			String filePath = "C:\\project3\\test\\WebContent\\assets\\img";
+			
+			if(iterator.hasNext()) {
+				imgFile = request.getFile(iterator.next());
+			}
+			
+			if(imgFile != null && imgFile.getOriginalFilename() != null) {
+				productvo.setProduct_img("/assets/img/product/" + imgFile.getOriginalFilename());
+				
+				String saveFilePath = filePath + File.separator + imgFile.getOriginalFilename();
+				imgFile.transferTo(new File(saveFilePath));
+			}
+		} catch(Exception e) {
+			System.out.println("e ---->>> " + e.getMessage());
+		}
+		
+		productvo.setProduct_address(request.getParameter("product_address"));
+		productvo.setProduct_content(request.getParameter("product_content"));
+		productvo.setProduct_hash_tag(request.getParameter("product_hash_tag"));
+		
+		int updateCnt = productService.updateProduct(productvo);
+		
+		if(updateCnt > 0) {
+			return "redirect:/product/edit.do?product_idx=" + productvo.getProduct_idx();
+		} else {
+			return "redirect:/product/edit.do?product_idx=" + productvo.getProduct_idx();
+		}
+	}
+	
+	@RequestMapping(value = "/product/delete_proc.do")
+	public String productDeleteProc(
+		HttpServletRequest request, 
+		HttpServletResponse response, 
+		HttpSession session, 
+		ModelMap model, 
+		@ModelAttribute("productVO") ProductVO productvo 
+	) throws Exception {
+		// 상품 조회수 업데이트
+		int updateCnt = productService.deleteProduct(productvo);
+		
+		if(updateCnt > 0) {
+			return "redirect:/product/list.do";
+		} else {
+			return "redirect:/product/list.do";
+		}
 	}
 	
 }
