4ec2ad87e0d9cc3151e1636a4f7567d64bf86fe5
kjk4867 <127995835+kjk4867@users.noreply.github.com>
2023-06-06 00:10:43 +0900

    Update ProductVO.java

--- backend2/src/main/java/test/product/service/ProductVO.java
+++ backend2/src/main/java/test/product/service/ProductVO.java
@@ -1,36 +1,36 @@
 package test.product.service;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-
 import test.example.sample.service.DefaultVO;
 
 public class ProductVO extends DefaultVO {
 	
 	private static final long serialVersionUID = -3648864362909943790L;
-
-	private int product_idx;
 	
-	private String product_name;
+	private int product_idx; // int
 	
-	private String product_eng_name;
+	private String product_name; // varchar(45)
 	
-	private String product_day_bak;
+	private int member_id; // int
 	
-	private String product_price;
+	private String nickname; // int
 	
-	private String product_img;
+	private String product_price; // int
 	
-	private String product_address;
+	private String product_img; // text
 	
-	private String product_content;
+	private String product_address; // text
 	
-	private String product_hash_tag;
+	private String product_content; // text
 	
-	private Double review_score_avg;
+	private String precaution; // text
 	
-	private String reg_date;
+	private String product_hash_tag; // text
 	
-	private String edit_date;
+	private int hit_cnt; // int
+	
+	private String created_date;
+	
+	private String updated_date;
 	
 	private String product_status;
 	
@@ -38,24 +38,22 @@ public class ProductVO extends DefaultVO {
 	
 	private int review_cnt;
 	
-	private int review_score;
+	private int rental_review_score;
 	
-	private String review_content;
+	private String rental_review_content;
 	
-	private String review_writer;
+	private String rental_review_score_1_per;
 	
-	private String product_review_status;
+	private String rental_review_score_2_per;
 	
-	private String review_score_1_per;
+	private String rental_review_score_3_per;
 	
-	private String review_score_2_per;
+	private String rental_review_score_4_per;
 	
-	private String review_score_3_per;
+	private String rental_review_score_5_per;
 	
-	private String review_score_4_per;
+	private int rental_review_score_avg;
 	
-	private String review_score_5_per;
-
 	public int getProduct_idx() {
 		return product_idx;
 	}
@@ -72,20 +70,20 @@ public class ProductVO extends DefaultVO {
 		this.product_name = product_name;
 	}
 	
-	public String getProduct_eng_name() {
-		return product_eng_name;
+	public int getMember_id() {
+		return member_id;
+	}
+	
+	public void setMember_id(int member_id) {
+		this.member_id = member_id;
+	}
+	
+	public String getNickname() {
+		return nickname;
 	}
 
-	public void setProduct_eng_name(String product_eng_name) {
-		this.product_eng_name = product_eng_name;
-	}
-
-	public String getProduct_day_bak() {
-		return product_day_bak;
-	}
-
-	public void setProduct_day_bak(String product_day_bak) {
-		this.product_day_bak = product_day_bak;
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
 	}
 
 	public String getProduct_price() {
@@ -99,57 +97,57 @@ public class ProductVO extends DefaultVO {
 	public String getProduct_img() {
 		return product_img;
 	}
-
+	
 	public void setProduct_img(String product_img) {
 		this.product_img = product_img;
 	}
-
+	
 	public String getProduct_address() {
 		return product_address;
 	}
-
+	
 	public void setProduct_address(String product_address) {
 		this.product_address = product_address;
 	}
-
+	
 	public String getProduct_content() {
 		return product_content;
 	}
-
+	
 	public void setProduct_content(String product_content) {
 		this.product_content = product_content;
 	}
-
+	
+	public String getPrecaution() {
+		return precaution;
+	}
+	
+	public void setPrecaution(String precaution) {
+		this.precaution = precaution;
+	}
+	
 	public String getProduct_hash_tag() {
 		return product_hash_tag;
 	}
-
+	
 	public void setProduct_hash_tag(String product_hash_tag) {
 		this.product_hash_tag = product_hash_tag;
 	}
-
-	public Double getReview_score_avg() {
-		return review_score_avg;
+	
+	public String getCreated_date() {
+		return created_date;
 	}
 	
-	public void setReview_score_avg(Double review_score_avg) {
-		this.review_score_avg = review_score_avg;
+	public void setCreated_date(String created_date) {
+		this.created_date = created_date;
 	}
 	
-	public String getReg_date() {
-		return reg_date;
+	public String getUpdated_date() {
+		return updated_date;
 	}
 	
-	public void setReg_date(String reg_date) {
-		this.reg_date = reg_date;
-	}
-	
-	public String getEdit_date() {
-		return edit_date;
-	}
-	
-	public void setEdit_date(String edit_date) {
-		this.edit_date = edit_date;
+	public void setUpdated_date(String updated_date) {
+		this.updated_date = updated_date;
 	}
 	
 	public String getProduct_status() {
@@ -171,85 +169,90 @@ public class ProductVO extends DefaultVO {
 	public int getReview_cnt() {
 		return review_cnt;
 	}
-
+	
 	public void setReview_cnt(int review_cnt) {
 		this.review_cnt = review_cnt;
 	}
-
-	public int getReview_score() {
-		return review_score;
+	
+	public int getRental_review_score() {
+		return rental_review_score;
 	}
 	
-	public void setReview_score(int review_score) {
-		this.review_score = review_score;
+	public void setReview_score(int rental_review_score) {
+		this.rental_review_score = rental_review_score;
 	}
 	
-	public String getReview_content() {
-		return review_content;
+	public String getRental_review_content() {
+		return rental_review_content;
 	}
 	
-	public void setReview_content(String review_content) {
-		this.review_content = review_content;
+	public void setReview_content(String rental_review_content) {
+		this.rental_review_content = rental_review_content;
+	}
+
+	public int getHit_cnt() {
+		return hit_cnt;
+	}
+
+	public void setHit_cnt(int hit_cnt) {
+		this.hit_cnt = hit_cnt;
+	}
+
+	public String getRental_review_score_1_per() {
+		return rental_review_score_1_per;
+	}
+
+	public void setRental_review_score_1_per(String rental_review_score_1_per) {
+		this.rental_review_score_1_per = rental_review_score_1_per;
+	}
+
+	public String getRental_review_score_2_per() {
+		return rental_review_score_2_per;
+	}
+
+	public void setRental_review_score_2_per(String rental_review_score_2_per) {
+		this.rental_review_score_2_per = rental_review_score_2_per;
+	}
+
+	public String getRental_review_score_3_per() {
+		return rental_review_score_3_per;
+	}
+
+	public void setRental_review_score_3_per(String rental_review_score_3_per) {
+		this.rental_review_score_3_per = rental_review_score_3_per;
+	}
+
+	public String getRental_review_score_4_per() {
+		return rental_review_score_4_per;
+	}
+
+	public void setRental_review_score_4_per(String rental_review_score_4_per) {
+		this.rental_review_score_4_per = rental_review_score_4_per;
+	}
+
+	public String getRental_review_score_5_per() {
+		return rental_review_score_5_per;
+	}
+
+	public void setRental_review_score_5_per(String rental_review_score_5_per) {
+		this.rental_review_score_5_per = rental_review_score_5_per;
+	}
+
+	public void setRental_review_score(int rental_review_score) {
+		this.rental_review_score = rental_review_score;
+	}
+
+	public void setRental_review_content(String rental_review_content) {
+		this.rental_review_content = rental_review_content;
+	}
+
+	public int getRental_review_score_avg() {
+		return rental_review_score_avg;
+	}
+
+	public void setRental_review_score_avg(int rental_review_score_avg) {
+		this.rental_review_score_avg = rental_review_score_avg;
 	}
 	
-	public String getReview_writer() {
-		return review_writer;
-	}
 	
-	public void setReview_writer(String review_writer) {
-		this.review_writer = review_writer;
-	}
-	
-	public String getProduct_review_status() {
-		return product_review_status;
-	}
-	
-	public void setProduct_review_status(String product_review_status) {
-		this.product_review_status = product_review_status;
-	}
-	
-	public String getReview_score_1_per() {
-		return review_score_1_per;
-	}
-
-	public void setReview_score_1_per(String review_score_1_per) {
-		this.review_score_1_per = review_score_1_per;
-	}
-
-	public String getReview_score_2_per() {
-		return review_score_2_per;
-	}
-
-	public void setReview_score_2_per(String review_score_2_per) {
-		this.review_score_2_per = review_score_2_per;
-	}
-
-	public String getReview_score_3_per() {
-		return review_score_3_per;
-	}
-
-	public void setReview_score_3_per(String review_score_3_per) {
-		this.review_score_3_per = review_score_3_per;
-	}
-
-	public String getReview_score_4_per() {
-		return review_score_4_per;
-	}
-
-	public void setReview_score_4_per(String review_score_4_per) {
-		this.review_score_4_per = review_score_4_per;
-	}
-
-	public String getReview_score_5_per() {
-		return review_score_5_per;
-	}
-
-	public void setReview_score_5_per(String review_score_5_per) {
-		this.review_score_5_per = review_score_5_per;
-	}
-
-	@Override
-	public String toString() {
-		return ToStringBuilder.reflectionToString(this);
-	}
 }
